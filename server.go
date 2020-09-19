0fc81001fb7e0f4809c999f15ff0720ab3af552c
Adisorn Sangangsumalee <adisorn.sa@kbtg.tech>
2020-09-19 16:25:33 +0700

    change to DI

--- server.go
+++ server.go
@@ -19,7 +19,12 @@ type Pair struct {
 }
 
 //PairDeviceHandler ...
-func PairDeviceHandler(w http.ResponseWriter, r *http.Request) {
+type PairDeviceHandler struct {
+	createPairDevice CreatePairDevice
+}
+
+//ServeHTTP ...
+func (ph *PairDeviceHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	var p Pair
 
 	err := json.NewDecoder(r.Body).Decode(&p)
@@ -31,19 +36,7 @@ func PairDeviceHandler(w http.ResponseWriter, r *http.Request) {
 
 	defer r.Body.Close()
 
-	db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
-	//db, err := sql.Open("postgres", "postgres://gosctihb:CqOz6dVYlooEBPY4quY9KHvySa2OmADZ@arjuna.db.elephantsql.com:5432/gosctihb")
-
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		json.NewEncoder(w).Encode(err.Error())
-		return
-	}
-
-	defer db.Close()
-
-	_, err = db.Exec("INSERT INTO pairs VALUES ($1,$2);", p.DeviceID, p.UserID)
-
+	err = ph.createPairDevice(p)
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		json.NewEncoder(w).Encode(err.Error())
@@ -54,11 +47,33 @@ func PairDeviceHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte(`{"status":"active"}`))
 }
 
+//CreatePairDevice ...
+type CreatePairDevice func(p Pair) error
+
+func createPairDevice(p Pair) error {
+	db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
+	//db, err := sql.Open("postgres", "postgres://gosctihb:CqOz6dVYlooEBPY4quY9KHvySa2OmADZ@arjuna.db.elephantsql.com:5432/gosctihb")
+
+	if err != nil {
+		log.Fatal("connect to database error", err)
+	}
+
+	defer db.Close()
+
+	_, err = db.Exec("INSERT INTO pairs VALUES ($1,$2);", p.DeviceID, p.UserID)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
 func main() {
 	fmt.Println("hello hometic : I'm Gopher!!")
 
 	r := mux.NewRouter()
-	r.HandleFunc("/pair-device", PairDeviceHandler).Methods(http.MethodPost)
+	r.Handle("/pair-device", &PairDeviceHandler{createPairDevice: createPairDevice}).Methods(http.MethodPost)
 
 	addr := fmt.Sprintf("0.0.0.0:%s", os.Getenv("PORT"))
 
--- server_test.go
+++ server_test.go
@@ -14,7 +14,21 @@ func TestCreatePairDevice(t *testing.T) {
 	req := httptest.NewRequest(http.MethodPost, "/pair-device", payload)
 	rec := httptest.NewRecorder()
 
-	PairDeviceHandler(rec, req)
+	/*
+		origin := createPairDevice
+		defer func() {
+			createPairDevice = origin
+		}()
+		createPairDevice = func(p Pair) error {
+			return nil
+		}
+	*/
+
+	handler := &PairDeviceHandler{createPairDevice: func(p Pair) error {
+		return nil
+	}}
+
+	handler.ServeHTTP(rec, req)
 
 	if http.StatusOK != rec.Code {
 		t.Error("expect 200 OK but got ", rec.Code)
@@ -28,3 +42,4 @@ func TestCreatePairDevice(t *testing.T) {
 }
 
 //go test
+//go test -v
